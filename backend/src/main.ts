ebd326df471fc4a2c62b3f955faa31324c3b7eb7
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-13 18:25:31 +0200

    graphql path included in the server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import yoga from "./utils/yoga";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use("/graphql", yoga);
+
   await app.listen(3000);
 }
 bootstrap();
