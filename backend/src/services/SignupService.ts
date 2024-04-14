f093a1afc76ede9fab9073443ff338f48efbea8f
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-14 19:52:48 +0200

    signup service

--- backend/src/services/SignupService.ts
+++ backend/src/services/SignupService.ts
@@ -1,44 +1,47 @@
-// import { Injectable } from "@nestjs/common";
-// import { PrismaService } from "./prismaService"; // Handle DB interactions
-// import { KafkaService } from "./kafkaService"; // Handle Kafka interactions
-// import bcrypt from "bcrypt";
-// import { UserRole } from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prismaService"; // Handle DB interactions
+import { KafkaService } from "./kafkaService"; // Handle Kafka interactions
+import bcrypt from "bcrypt";
+import { UserRole } from "@prisma/client";
 
-// @Injectable()
-// export class AppService {
-//   constructor(
-//     private prisma: PrismaService,
-//     private kafka: KafkaService,
-//   ) {}
+@Injectable()
+export class SignupService {
+  constructor(
+    private prisma: PrismaService,
+    private kafka: KafkaService,
+  ) {}
 
-//   async registerUser(userData: {
-//     email: string;
-//     password: string;
-//     role: UserRole;
-//   }): Promise<boolean> {
-//     const { email, password, role } = userData;
+  async registerUser(userData: {
+    email: string;
+    password: string;
+    role: UserRole;
+  }): Promise<boolean> {
+    const { email, password, role } = userData;
 
-//     const existingUser = await this.prisma.user.findUnique({
-//       where: { email },
-//     });
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
 
-//     if (existingUser) {
-//       throw new Error("User already exists.");
-//     }
+    try {
+      if (existingUser) {
+        throw new Error("User already exists.");
+      }
 
-//     const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          role, // Assuming the schema has 'role' field
+        },
+      });
 
-//     const user = await this.prisma.user.create({
-//       data: {
-//         email,
-//         password: hashedPassword,
-//         UserRole: role,
-//       },
-//     });
-
-//     // Assuming successful user creation
-//     await this.kafka.publish("user-created", { userId: user.id });
-
-//     return true;
-//   }
-// }
+      await this.kafka.publish("user-created", [{ userId: user.id }]);
+      return true;
+    } catch (error) {
+      console.error("Registration error:", error);
+      throw new Error("Failed to register user.");
+    }
+  }
+}
+export default SignupService;
