310939cde838e25c6f88dd8d0e5b23708e1133d4
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-14 19:52:27 +0200

    signup service added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from "@nestjs/common";
 import { ClientsModule, Transport } from "@nestjs/microservices";
 import { PrismaService } from "./services/PrismaService";
 import { KafkaService } from "./services/KafkaService";
+import { SignupService } from "./services/SignupService";
 @Module({
-  providers: [PrismaService, KafkaService],
+  providers: [PrismaService, SignupService, KafkaService],
 })
 export class AppModule {}
