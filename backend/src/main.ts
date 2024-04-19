5420e72b16dabdd6794e29080b5085a39426a4bc
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-19 03:22:20 +0200

    env added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 //import yoga from "./utils/yoga";
-//import * as dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
-//dotenv.config(); // Make sure this is at the top to load environment variables early
+dotenv.config(); // Make sure this is at the top to load environment variables early
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
