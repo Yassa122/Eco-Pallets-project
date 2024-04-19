9bafde9a8d37ce1971c80dc8ec77eec45879da4c
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-19 03:22:43 +0200

    added env in the auth module

--- backend/src/modules/auth.module.ts
+++ backend/src/modules/auth.module.ts
@@ -6,11 +6,16 @@ import { PrismaModule } from "./prisma.module";
 import { JwtModule } from "@nestjs/jwt/dist/jwt.module";
 import { GraphQLModule } from "@nestjs/graphql";
 import { join } from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 // src/auth/auth.module.ts
 @Module({
   imports: [
     PrismaModule, // Ensures PrismaService is available
-    JwtModule, // Configure JWT Module as required
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
     PassportModule, // Passport configuration
   ],
   providers: [AuthService, JwtStrategy],
