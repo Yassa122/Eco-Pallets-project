ae6d9fa9b6a9e41ee4517b6c0bdc5818906a8f68
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-20 02:48:10 +0200

    updated tailwind config

--- plastic-pallets/tailwind.config.ts
+++ plastic-pallets/tailwind.config.ts
@@ -2,19 +2,26 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./public/**/*.html",
+    "./src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "dark-grey": "#1B1B1B",
+      },
       backgroundImage: {
+        "text-gradient":
+          "linear-gradient(to bottom right, #E1CA79 30%, #BCC0C3 70%, #707070 80%)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 export default config;
