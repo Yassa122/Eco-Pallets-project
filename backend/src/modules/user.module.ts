31c2ee8f67f592638e076137c39a696554b1e16b
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-22 23:44:11 +0200

    user module

--- backend/src/modules/user.module.ts
+++ backend/src/modules/user.module.ts
@@ -1,9 +1,10 @@
 // src/user/user.module.ts
 import { Module } from "@nestjs/common";
-import { UserResolver } from "../resolvers/mutations/user.resolver";
+import { UserResolver } from "../resolvers/mutations/resolvers/user.resolver";
 import { AuthModule } from "./auth.module"; // Import AuthModule here
+import { UserService } from "services/user.service";
 @Module({
   imports: [AuthModule], // Import AuthModule here
-  providers: [UserResolver],
+  providers: [UserResolver, UserService],
 })
 export class UserModule {}
