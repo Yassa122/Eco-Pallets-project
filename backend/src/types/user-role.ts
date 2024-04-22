8a049592bc2a3a357fe68936ddfb6f814c93fee5
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-22 23:43:47 +0200

    removed folder/ dir update

--- backend/src/types/user-role.ts
+++
@@ -1,8 +0,0 @@
-import { enumType } from "nexus";
-
-const UserRoleEnum = enumType({
-  name: "UserRole",
-  members: ["USER", "user", "ADMIN", "MANAGER", "GUEST"], // Adjusted to match Prisma enum
-});
-
-export default UserRoleEnum;
