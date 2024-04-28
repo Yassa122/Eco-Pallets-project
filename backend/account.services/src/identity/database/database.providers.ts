b6814be60a593637a3f0eee361873bd0f8287c9c
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-28 23:40:59 +0300

    db provider

--- backend/account.services/src/identity/database/database.providers.ts
+++ backend/account.services/src/identity/database/database.providers.ts
@@ -4,8 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        'mongodb://127.0.0.1:27017/plastic-pallets',
-      ), // Updated database name
+      mongoose.connect('mongodb://127.0.0.1:27017/plastic-pallets'), // Updated database name
   },
 ];
