e249e7f97ed734be01fc6aed6e2ecd0f2eef3e11
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-28 23:36:55 +0300

    account.controller

--- backend/api-gateway/src/account/account.controller.ts
+++ backend/api-gateway/src/account/account.controller.ts
@@ -1,33 +1,40 @@
-import { Controller, Request,Get, Inject, OnModuleInit, Post } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Get,
+  ParseFileOptions,
+  Inject,
+  OnModuleInit,
+  Post,
+} from '@nestjs/common';
 import { AccountService } from './account.service';
 import { ClientKafka } from '@nestjs/microservices';
 
 @Controller('account')
 export class AccountController implements OnModuleInit {
+  constructor(
+    private accountServices: AccountService,
+    @Inject('ACC_SERVICE') private readonly accountClient: ClientKafka,
+  ) {}
 
-    constructor(private accountServices:AccountService,
-        @Inject('ACC_SERVICE') private readonly accountClient:ClientKafka){}
-   
+  @Get('hello')
+  getHello(): any {
+    return this.accountServices.hello();
+  }
 
-    @Get('hello')
-    getHello():any{
-        return this.accountServices.hello();
-    }
+  @Post('sign-up')
+  async register(@Request() req) {
+    return this.accountServices.register({ body: req.body.data });
+  }
 
-    @Post('sign-up')
-    async regster(@Request()req){
-        return this.accountServices.register({body:req.body.data});
-    }
+  @Post('sign-in')
+  async login(@Request() req) {
+    return this.accountServices.login({ body: req.body.data });
+  }
 
-    @Post('sign-in')
-    async login(@Request()req){
-        return this.accountServices.login({body:req.body.data});
-    }
-
-    onModuleInit() {
-        this.accountClient.subscribeToResponseOf('hellofromapi');
-        this.accountClient.subscribeToResponseOf('register');
-        this.accountClient.subscribeToResponseOf('login');
-    }
-
+  onModuleInit() {
+    this.accountClient.subscribeToResponseOf('hellofromapi');
+    this.accountClient.subscribeToResponseOf('register');
+    this.accountClient.subscribeToResponseOf('login');
+  }
 }
