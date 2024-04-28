d67f321c42eb5e29d0a19cfb3123f3c2c324dc41
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-28 23:39:25 +0300

    added user module

--- backend/account.services/src/app.module.ts
+++ backend/account.services/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdentityModule } from './identity/identity.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './identity/users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/plastic-pallets'),
     IdentityModule,
+    UsersModule ,
+
   ],
   controllers: [AppController],
   providers: [AppService],
