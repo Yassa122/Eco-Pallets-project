35941221cfaea4f75b5cc3eb9c1e9d6bf113b9d9
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-29 02:04:28 +0300

    added user schema

--- backend/api-gateway/src/app.service.ts
+++ backend/api-gateway/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ClientKafka, ClientProxy, Transport } from '@nestjs/microservices';
-import {}
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  
-  
 }
