9e2c5d82e897d23d8a9da055fe2d14b96e9e1a14
Yassa122 <yassa.ashraf@yahoo.com>
2024-04-29 12:52:03 +0300

    added the identityService to call it in the app.service

--- backend/account.services/src/app.module.ts
+++ backend/account.services/src/app.module.ts
@@ -4,14 +4,15 @@ import { AppService } from './app.service';
 import { IdentityModule } from './identity/identity.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './identity/users/users.module';
+import { IdentityService } from './identity/identity.service';
+import { JwtService } from '@nestjs/jwt';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/plastic-pallets'),
     IdentityModule,
-    UsersModule ,
-
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, IdentityService, JwtService],
 })
 export class AppModule {}
