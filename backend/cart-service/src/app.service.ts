d024ccf7ed8550b5b5c0457b05ef2e4b020f494a
FadyIbrahim123 <134152609+FadyIbrahim123@users.noreply.github.com>
2024-05-01 22:44:08 +0300

    added create cart to service

---
+++ backend/cart-service/src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose';
+import { CreateCartDto } from './dto/cart.dto'; // Assuming CreateCartDto is defined in a separate file
+
+@Injectable()
+export class AppService {
+  constructor(@InjectModel('Cart') private readonly cartModel: Model<any>) {}
+
+  getHello(): string {
+    return 'Hello!';
+  }
+
+  async createCart(createCartDto: CreateCartDto): Promise<CreateCartDto> {
+    const createdCart = new this.cartModel(createCartDto);
+    return createdCart.save();
+  }
+  asadsasd
+}
