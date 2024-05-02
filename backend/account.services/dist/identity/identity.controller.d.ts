4f078c5b9376ecc47aeafe3399d7ba6c409a2eb9
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-03 02:41:51 +0300

    removed dist folder

--- backend/account.services/dist/identity/identity.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { IdentityService } from './identity.service';
-export declare class IdentityController {
-    private identityService;
-    constructor(identityService: IdentityService);
-    hello(req: any): any;
-    register(command: any): Promise<import("./interfaces/user").User>;
-    login(command: any): Promise<any>;
-    me(command: any): Promise<any>;
-}
