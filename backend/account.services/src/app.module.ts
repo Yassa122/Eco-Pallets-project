890924fede34577a58559e1abca328e3246d5314
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-05 23:28:51 +0300

    added kafka module and kafka service

--- backend/account.services/src/app.module.ts
+++ backend/account.services/src/app.module.ts
@@ -6,13 +6,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './identity/users/users.module';
 import { IdentityService } from './identity/identity.service';
 import { JwtService } from '@nestjs/jwt';
+import { KafkaModule } from './kafka/kafka.module';
+import { UpdateUserProfileDto } from './identity/dto/updateUserProfile.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { KafkaService } from './kafka/kafka.service';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/plastic-pallets'),
     IdentityModule,
     UsersModule,
+    KafkaModule,
+
   ],
   controllers: [AppController],
-  providers: [AppService, IdentityService, JwtService],
+  providers: [AppService, IdentityService, JwtService,KafkaService],
 })
 export class AppModule {}
