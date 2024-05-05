b3ebc52291349c358d0cc2fd957aaa5654370183
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-05 23:32:08 +0300

    modified exists strategy

--- backend/account.services/src/identity/strategies/exists.strategy.ts
+++ backend/account.services/src/identity/strategies/exists.strategy.ts
@@ -1,29 +1,21 @@
-import { Strategy } from "passport-local";
-import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { IdentityService } from "../identity.service";
-import { LoginDto } from "../dto/login.dto";
-import { UserAlreadyExistsException } from "../exceptions/userAlreadyExists.exception";
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { IdentityService } from '../identity.service';
+import { LoginDto } from '../dto/login.dto';
+import { UserAlreadyExistsException } from '../exceptions/userAlreadyExists.exception';
 
 @Injectable()
-export class ExistsStrategy extends PassportStrategy(Strategy, 'exists'){
-constructor(private readonly identityService:IdentityService){
+export class ExistsStrategy extends PassportStrategy(Strategy, 'exists') {
+  constructor(private readonly identityService: IdentityService) {
     super();
-}
-async validate(username:string, password:string):Promise<any>{
-    console.log(username,password);
+  }
+  async validate(username: string, password: string): Promise<any> {
+    console.log(username, password);
 
-    var loginDto:LoginDto ={
-        username,password
-    }
-    const user = await this.identityService.getUserbyUsername(username);
-
-    if(!user||user !==null){
-        throw new UserAlreadyExistsException();
-    }
-    return {
-        username:username,
-        pasword:password,
+    var loginDto: LoginDto = {
+      username,
+      password,
     };
-}
+  }
 }
