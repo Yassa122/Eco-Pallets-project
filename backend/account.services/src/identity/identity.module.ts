3d077d65b3376463184be35a8e8d53ce70197185
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-06 17:12:27 +0300

    update and get user are working

--- backend/account.services/src/identity/identity.module.ts
+++ backend/account.services/src/identity/identity.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { IdentityController } from './identity.controller';
 import { IdentityService } from './identity.service';
-import { databaseProviders } from './database/database.providers';
-import { identityProviders } from './database/identity.providers';
+import { databaseProviders } from '../database/database.providers';
+import { identityProviders } from '../database/identity.providers';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local.strategy';
