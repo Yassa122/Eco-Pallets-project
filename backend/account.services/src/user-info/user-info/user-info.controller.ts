7588e5882478b0ee4425634197256d9524ce53eb
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-09 18:22:21 +0300

    user now is not required to to put the id instead it is saved in the token

--- backend/account.services/src/user-info/user-info/user-info.controller.ts
+++ backend/account.services/src/user-info/user-info/user-info.controller.ts
@@ -1,40 +1,50 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { UserInfoService } from './user-info.service';
-import { GetUserDto } from 'src/user-info/dto/get-user.dto';
+import { GetUserId } from '../../decorators/get-user-id.decorator';
 import { AddShippingAddressDto } from '../dto/add-shipping-address.dto';
 import { UpdateShippingAddressDto } from '../dto/update-shipping-address.dto';
 import { DeleteShippingAddressDto } from '../dto/delete-shipping-address.dto';
 
 @Controller('user-info')
+@UseGuards(AuthGuard('jwt')) // Apply JWT Auth Guard to all routes in the controller
 export class UserInfoController {
-constructor(private UserInfoService: UserInfoService) {}
+  constructor(private userInfoService: UserInfoService) {}
 
-  @Get(':id')
-  getUser(@Param('id') id: string) {
-    return this.UserInfoService.getUserData(id);
+  @Get('addresses')
+  getShippingAddresses(@GetUserId('userId') userId: string) {
+    return this.userInfoService.getShippingAddresses(userId);
   }
 
-  @Put('update/:id')
-  updateUser(@Param('id') id: string, @Body() userData: GetUserDto) {
-    return this.UserInfoService.updateUserData(id, userData);
-  }
-  @Post('add-address/:userId')
-    addShippingAddress(@Param('userId') userId: string, @Body() addressDto: AddShippingAddressDto) {
-        return this.UserInfoService.addShippingAddress(userId, addressDto);
-    }
-
-    @Put('update-address/:userId')
-    updateShippingAddress(@Param('userId') userId: string, @Body() updateDto: UpdateShippingAddressDto) {
-        return this.UserInfoService.updateShippingAddress(userId, updateDto);
-    }
-
-    @Delete('delete-address/:userId')
-    deleteShippingAddress(@Param('userId') userId: string, @Body() deleteDto: DeleteShippingAddressDto) {
-    return this.UserInfoService.deleteShippingAddress(userId, deleteDto._id);
+  @Post('add-address')
+  addShippingAddress(
+    @GetUserId('userId') userId: string,
+    @Body() addressDto: AddShippingAddressDto,
+  ) {
+    return this.userInfoService.addShippingAddress(userId, addressDto);
   }
 
-    @Get('addresses/:userId')
-    getShippingAddresses(@Param('userId') userId: string) {
-        return this.UserInfoService.getShippingAddresses(userId);
-    }
+  @Put('update-address')
+  updateShippingAddress(
+    @GetUserId('userId') userId: string,
+    @Body() updateDto: UpdateShippingAddressDto,
+  ) {
+    return this.userInfoService.updateShippingAddress(userId, updateDto);
+  }
+
+  @Delete('delete-address')
+  deleteShippingAddress(
+    @GetUserId('userId') userId: string,
+    @Body() deleteDto: DeleteShippingAddressDto,
+  ) {
+    return this.userInfoService.deleteShippingAddress(userId, deleteDto._id);
+  }
 }
