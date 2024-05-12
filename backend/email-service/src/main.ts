e43a9605ea257e9c1ea2a0dea25e459de49064da
amirwessam <121111555+amirwessam@users.noreply.github.com>
2024-05-12 23:47:32 +0300

    added email service

---
+++ backend/email-service/src/main.ts
@@ -0,0 +1,10 @@
+//server is running here...
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8000);
+}
+bootstrap();
