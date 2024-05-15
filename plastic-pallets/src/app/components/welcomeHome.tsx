31a17a09e96d027a99db3168abc3f05560e7bff3
georgeY2002 <georgeyoussef2002@gmail.com>
2024-05-15 15:57:15 +0300

    modified files nth working

---
+++ plastic-pallets/src/app/components/welcomeHome.tsx
@@ -0,0 +1,16 @@
+const WelcomeHome = () => {
+    return (
+      <div className="welcome-home">
+        <div className="authorized">100% Authorized</div>
+        <div className="slogan">YOUR BEST PLASTIC PALLET MADE WORLD-WIDE</div>
+        <div className="since">Since 1991</div>
+        <div className="subscribe">
+          <input type="email" placeholder="Your Email" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default WelcomeHome;
+  
---
+++ plastic-pallets/src/app/pages/home/HomePage.tsx
@@ -0,0 +1,18 @@
+import Navbar from '../../components/navbar';
+import FeaturedProducts from '../../components/FeaturedProducts';
+import WelcomeHome from '../../components/welcomeHome'; // Corrected import and component name
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <WelcomeHome /> {/* Capitalized component name */}
+      <main>
+        <h1>Welcome to Our Store!</h1>
+        <FeaturedProducts />
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
