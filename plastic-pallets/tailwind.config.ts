4d2dce68b6cd999cbb56de5d186d3be60776d802
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-17 19:22:13 +0300

    tailwind config

--- plastic-pallets/tailwind.config.ts
+++ plastic-pallets/tailwind.config.ts
@@ -46,11 +46,7 @@ const config: Config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+    
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
