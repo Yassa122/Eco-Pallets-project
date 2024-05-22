0a68d2d83533b110db47b903bec180d569b2bf54
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-22 21:33:40 +0300

    asas

--- plastic-pallets/src/app/pages/cart/page.tsx
+++ plastic-pallets/src/app/pages/cart/page.tsx
@@ -1,18 +1,36 @@
-"use client";
+import Link from "next/link";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import "./global.css";
+import Cart from "@/components/cart/page"; // Adjust the import path as needed
+import PlaceholderContent from "@/components/demo/placeholder-content"; // Adjust the import path as needed
 
-import Navbar from "../../components/cartComponents/navbar";
-import PhasesComponent from "../../components/cartComponents/phases";
-import ShoppingCart from "@/app/components/cartComponents/cartItems";
-import Proceed from "@/app/components/cartComponents/proceed";
-import Image from "next/image";
-export default function Cart() {
+export default function DashboardPage() {
   return (
-    <main>
-      <Navbar />
-      <PhasesComponent />
-      <div className="p-16">
-      <ShoppingCart />
-</div>
-    </main>
+    <ContentLayout title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent>
+        <Cart /> {/* Cart component is rendered inside PlaceholderContent */}
+      </PlaceholderContent>
+    </ContentLayout>
   );
 }
