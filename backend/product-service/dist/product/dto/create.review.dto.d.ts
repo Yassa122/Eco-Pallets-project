11272f1c504c902b3db85a984968c563b2aa4049
georgeY2002 <georgeyoussef2002@gmail.com>
2024-05-25 01:57:06 +0300

    removed dist

--- backend/product-service/dist/product/dto/create.review.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateReviewDto {
-    rating: number;
-    comment: string;
-}
--- backend/product-service/dist/product/dto/customization.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomizationDto = void 0;
-class CustomizationDto {
-}
-exports.CustomizationDto = CustomizationDto;
-//# sourceMappingURL=customization.dto.js.map
