ad8976794107059bdafeeca1c7f3b4a3930b6bb1
georgeY2002 <georgeyoussef2002@gmail.com>
2024-05-25 01:55:34 +0300

    removed dist

--- backend/product-service/dist/product/dto/wishlist.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateWishlistDto = void 0;
-class CreateWishlistDto {
-}
-exports.CreateWishlistDto = CreateWishlistDto;
-//# sourceMappingURL=wishlist.dto.js.map
