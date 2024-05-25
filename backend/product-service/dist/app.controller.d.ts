f89244b2d3a4a82ace0c41633a410535e1b35a29
georgeY2002 <georgeyoussef2002@gmail.com>
2024-05-25 03:35:30 +0300

    removed

--- backend/product-service/dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AppService } from './app.service';
-import { ProductService } from './product/product.service';
-export declare class AppController {
-    private readonly appService;
-    private readonly productService;
-    constructor(appService: AppService, productService: ProductService);
-    create(reqBody: any): Promise<any>;
-}
--- backend/product-service/dist/product/dto/rent-product.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class RentProductDto {
-    rentalStart: Date;
-    rentalEnd: Date;
-    deposit: number;
-}
