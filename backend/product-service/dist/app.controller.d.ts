2763fe62f730c41c1e15ed6bc5b994800d11369b
dinaa3 <dinadel31@gmail.com>
2024-05-25 04:57:28 +0300

    deleted dist

--- backend/product-service/dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from './app.service';
-import { ProductService } from './product/product.service';
-export declare class AppController {
-    private readonly appService;
-    private readonly productService;
-    constructor(appService: AppService, productService: ProductService);
-    getAllProducts(): Promise<() => Promise<import("./product/dto/create-product.dto").CreateProductDto[]>>;
-    create(reqBody: any): Promise<any>;
-}
