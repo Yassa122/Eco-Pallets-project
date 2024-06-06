6c0c08881af457d2c407508a0b72d596a105a547
Yassa122 <yassa.ashraf@yahoo.com>
2024-06-06 17:15:12 +0300

    profile and reviews by renwa

---
+++ plastic-pallets/src/app/pages/profile/profile-settings/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import PlaceholderContent from "@/components/demo/placeholder-content";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import Profile from "../../../components/profileComponent/profile";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Modal from "@/components/modalTest"; // Import Modal component
+import { useState, useEffect } from "react";
+
+export default function profile() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const searchParams = useSearchParams();
+  const success = searchParams.get("success"); // Accessing query parameter 'success'
+
+  return (
+    <ContentLayout title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent>
+        <Profile /> {/* Cart component is rendered inside PlaceholderContent */}
+      </PlaceholderContent>
+    </ContentLayout>
+  );
+}
